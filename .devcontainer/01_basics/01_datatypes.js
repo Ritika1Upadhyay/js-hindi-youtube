dd544bc35c58d8f8a47205e1d97eb1e185afc8ee
Ritika1Upadhyay <150675252+Ritika1Upadhyay@users.noreply.github.com>
2023-12-20 12:11:11 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,29 @@
+// "use strict"; treat all JS code as a newer version
+// alert (3+3) we are using nodejs, not browser
+console.log(3 +3)
+    + 
+    // 3); code readability should be high this is not write way to write the code 
+
+console.log("Ritika")
+
+let name = "Ritika"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// These are the premitive data types:-
+// number => 2 to power of 53
+// bigint => when numbers are huge we use bigint 
+// string => ""
+// boolean => true/false
+// null => it's an object and it's a standalone value
+// undefined => means value not defined yet
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof "age");
+console.log(typeof null); // It's an object 
+console.log(typeof undefined); // undefined 
+
--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,6 +2,19 @@ const accountId = 144553
 let accountEmail = "abc@gmail.com"
 var accountPassword = "12345"
 accountCity = "Varanasi"
+let accountState;
 
-// accountId = 2
-console.log("accountId");
+// accountId = 2 not allowed in const
+accountEmail = "hchchc@gmail.com"
+accountPassword = "121221"
+accountCity = "Jaipur"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
