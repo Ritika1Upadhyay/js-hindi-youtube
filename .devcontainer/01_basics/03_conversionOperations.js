5ef1a06a3abec10ffea7625a47920a5dcd74bd6c
Ritika1Upadhyay <150675252+Ritika1Upadhyay@users.noreply.github.com>
2023-12-20 13:06:52 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+// let score = 33 
+//let score = null
+//let score = undefined
+//let score = true
+//let score = false
+let score = "Ritika"
+//console.log(typeof score);
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// Notes:-
+// 33 => is easily convertable in number 33
+// 33abc => Nan (not a number)
+// true => 1; false => 0
+
+
+//let isLoggedIn = 1
+let isLoggedIn = "ritika"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => value come true
+// 0 => false
+// "" (empty value) => false
+// "ritika" => true  
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
